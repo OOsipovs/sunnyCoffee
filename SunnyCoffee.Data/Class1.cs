e56d41a4ed92b250b19b10600286320130d4f5c8
Olegs <olegs.osipovs@gmail.com>
2020-06-07 14:04:21 +0300

    Initial Commit

---
+++ SunnyCoffee.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SunnyCoffee.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ SunnyCoffee.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SunnyCoffee.Services
+{
+    public class Class1
+    {
+    }
+}
