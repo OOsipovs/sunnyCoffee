674fc587fd96de1e54a99e5606dc5d6b7b30d08e
Olegs <olegs.osipovs@gmail.com>
2020-08-22 17:45:57 +0300

    Initial layout added

---
+++ sunnycoffee.frontend/src/components/SunnyButton.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="btn-link">
+        <button @click="visitRoute">
+            <slot></slot>
+        </button>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+    import Component from 'vue-class-component';
+    import {Prop} from 'vue-property-decorator';
+
+    @Component({
+        name: 'SunnyButton',
+        components: {}
+    })
+    export default class SunnyButton extends Vue {
+        @Prop({required: false, type: String})
+        link?: string;
+        
+        visitRoute(){
+            this.$router.push(this.link);
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
